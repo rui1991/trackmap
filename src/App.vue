ec26a35820fc18d84936fc4be181d281a8ccb514
rui1991 <591069449@qq.com>
2019-07-12 17:53:17 +0800

    2019-07-12 更新

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,15 @@ html, body{
   width: 100%;
   height: 100%;
 }
-body, div, p, span, i{
+body, div, p, ul, li, span, i{
   margin:0;
   padding:0;
   box-sizing:border-box;
   -moz-box-sizing:border-box;
   -webkit-box-sizing:border-box;
+}
+.el-tree--highlight-current .el-tree-node.is-current>.el-tree-node__content{
+  background: #e2e2e2;
 }
 #app {
   width: 100%;
